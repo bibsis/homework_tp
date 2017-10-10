039919defe80483d6336d9c176999baba6318399
Bilqna <bborisova2000@gmail.com>
2017-10-10 12:50:11 -0700

    First project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,66 @@
+require 'csv' 
+require 'linear-regression' 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_sessions
+  
+def calc
+  app = params[:app].read
+  m = CSV.parse(app)
+  i = 0
+  sum = 0
+  while i < m.lenght do 
+  sum = sum + m[i][0].to_i
+  i+=1
+  end
+  render plain: '%.2f'%sum.ceil
+end
+
+def second 
+  app = params[:app].read
+  m = CSV.parse(app)
+  i = 0
+  sum = 0
+  while i < m.lenght do 
+  if m[i][2] % 2 != 0
+  sum = sum + m[i][1].to_i
+  end
+  i+=1
+  end
+  render plain: '%.2f'%sum.ceil
+end
+
+def thirty
+  app = params[:app].read
+  m = CSV.parse(app)
+  i = 0
+  p = 0
+  sum = 0
+  max = 0
+  while i < m.lenght do 
+   while p < 30 do 
+   sum = sum + m[i+p][0].to_i
+   p+=1
+   end
+  if(sum > max) max = sum end
+  i+=1
+  end
+  render plain: '%.2f'%max.ceil
+end
+
+def lin
+  app = params[:app].read
+  m = CSV.parse(app)
+  i = 1
+  k = 0
+  while(i <= m.lenght)
+  ind[i-1] = i
+  end
+  while(k < m.lenght)
+  values[k][0] = k
+  end
+  l = Regression::Linear.new(ind, values)
+  b = linear.intercept
+  a = linear.slope
+  render plain: "#{'%.6f' % a},#{'%.6f' % b}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post "/sums" => "application#calc"
+  post "/filters" => "application#second"
+  post "/intervals" => "application#thirty"
+  post "/lin_regressions" => "application#lin"
 end
